44abfd47e099737a6069cc3fba09cf58631f74a9
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-16 19:34:34 -0400

    added code to solve

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,11 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin){
+        if (l.includes(word)) {
+            return true;
+        }
     }
+    return false;
 }
 
 module.exports = wordSearch
